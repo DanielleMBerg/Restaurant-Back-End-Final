3708228c2ff317f6e7ec474780338cc30b51cf5f
Danielle Berg <124546091+DanielleMBerg@users.noreply.github.com>
2023-11-06 20:55:49 -0800

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } = parse(env("postgres://restaurant_app_backend_user:OwvRRfc7LRpfHYRtFKpj95bgAkMXRYpv@dpg-cl4qckc72pts739l0bn0-a/restaurant_app_backend"));
 
   return {
     connection: {
